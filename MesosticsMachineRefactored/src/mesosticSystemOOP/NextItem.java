c8415490b7f3694eacbba39db6ff6614de54a68b
Martin <Martin@MartinThinkPad>
2018-02-04 10:31:11 +0000

    Javadoc commentary added

--- MesosticsMachineRefactored/src/mesosticSystemOOP/NextItem.java
+++ MesosticsMachineRefactored/src/mesosticSystemOOP/NextItem.java
@@ -1,5 +1,26 @@
 package mesosticSystemOOP;
 
+
+/**
+ * @author Martin Dowling
+ * 
+ * This interface contains in the abstract the four basic methods
+ * for traversing the mesostic row and the target chapter, 
+ * selecting appropriate words from the target chapter 
+ * to create appropriate mesostics, and writing the content of same to a file.
+ * 
+ * Item() contains the appropriate algorithm for selecting a word from the target text.
+ * The concrete classes implementing this interface are:
+ * 
+ * NextWord, which selects the next appropriate word in the chapter
+ * NextMesostic, which selects all the words of one mesostic row
+ * NextChapter, which selects all the mesostics in one chapter
+ * 
+ * The "decorator" design pattern is used embellish these classes with
+ * syllable repositories and filters. See the interface NextItemFiltered.
+ * 
+ * 
+ */
 import java.io.IOException;
 import java.util.ArrayList;
 
